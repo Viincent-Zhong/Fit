57d85edc346a33ad7a9c5b235c947ef743d70416
Viincent-Zhong <vincent.zhong@epitech.eu>
2024-08-29 21:47:29 +0200

    [ADD] Setup Pinia store for the app

--- travel-planner/src/pages/Home/HomeView.vue
+++ travel-planner/src/pages/Home/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
+    <p>{{ name }}</p>
+    <p>{{ count }}</p>
+    <button @click="increment">Increment button</button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,5 +11,11 @@
 <script setup lang="ts">
 
 import HelloWorld from '@/components/HelloWorld.vue';
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia';
+
+const store = useCounterStore()
+const { name, count } = storeToRefs(store)
+const { increment } = store
 
 </script>
